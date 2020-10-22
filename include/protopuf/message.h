6d7c10ed2539b9ce6a6b54e835ff31e3cca29195
PragmaTwice <i@twice.moe>
2020-10-22 17:57:19 +0800

    message: add field and message

--- include/protopuf/message.h
+++ include/protopuf/message.h
@@ -35,8 +35,21 @@ namespace pp {
         repeated
     };
 
-    template <uint<4> N, coder C, attribute A = singular>
-    struct field {
+    template <attribute, typename T, template<typename...> typename>
+    struct field_container_impl {
+        using type = std::optional<T>;
+    };
+
+    template <typename T, template<typename> typename C>
+    struct field_container_impl<repeated, T, C> {
+        using type = C<T>;
+    };
+
+    template <attribute A, typename T, template<typename...> typename Container = std::vector>
+    using field_container = typename field_container_impl<A, T, Container>::type;
+
+    template <uint<4> N, coder C, attribute A = singular, template<typename...> typename Container = std::vector>
+    struct field : field_container<A, typename C::value_type, Container>{
         static constexpr uint<4> number = N;
 
         static constexpr uint<4> key = (N << 3) | wire_type<C>;
@@ -44,6 +57,89 @@ namespace pp {
         using coder = C;
 
         static constexpr attribute attr = A;
+
+        using base_type = field_container<A, typename C::value_type, Container>;
+
+        using base_type::base_type;
+    };
+
+    template <uint<4> N, typename T, attribute A = singular, template<typename...> typename Container = std::vector>
+    using integer_field = field<N, integer_coder<T>, A, Container>;
+
+    template <uint<4> N, typename T, attribute A = singular, template<typename...> typename Container = std::vector>
+    using varint_field = field<N, varint_coder<T>, A, Container>;
+
+    template <uint<4> N, typename T, attribute A = singular, template<typename...> typename Container = std::vector>
+    using float_field = field<N, float_coder<T>, A, Container>;
+
+    template <uint<4> N, typename T, attribute A = singular, template<typename...> typename Container = std::vector>
+    using array_field = field<N, array_coder<T>, A, Container>;
+
+    template <uint<4> N, typename T, attribute A = singular, template<typename...> typename Container = std::vector>
+    using basic_string_field = field<N, basic_string_coder<T>, A, Container>;
+
+    template <uint<4> N, attribute A = singular, template<typename...> typename Container = std::vector>
+    using string_field = field<N, string_coder, A, Container>;
+
+    template <uint<4> N, attribute A = singular, template<typename...> typename Container = std::vector>
+    using bytes_field = field<N, bytes_coder, A, Container>;
+
+    template <typename>
+    constexpr bool is_field = false;
+
+    template <uint<4> N, coder C, attribute A, template<typename...> typename Container>
+    constexpr bool is_field <field<N, C, A, Container>> = true;
+
+    template <typename T>
+    concept field_c = is_field<T>;
+
+    struct field_not_found;
+
+    template <uint<4>, field_c...>
+    struct field_selector_impl;
+
+    template <uint<4> I, field_c C, field_c... D>
+    struct field_selector_impl<I, C, D...> {
+        using type = std::conditional_t<I == C::number, C, typename field_selector_impl<I, D...>::type>;
+    };
+
+    template <uint<4> I>
+    struct field_selector_impl<I> {
+        using type = field_not_found;
+    };
+
+    template <uint<4> I, field_c... C>
+    using field_selector = typename field_selector_impl<I, C...>::type;
+
+    template <field_c ... T>
+    struct message : private T... {
+
+        message() = default;
+
+        explicit message(T&& ...v) : T(std::forward<T>(v))... {};
+
+        message(const message& other) : T(static_cast<const T&>(other))... {}
+
+        static constexpr uint<4> size = sizeof...(T);
+
+        template <uint<4> N>
+        constexpr decltype(auto) get() const {
+            return static_cast<const field_selector<N, T...>&>(*this);
+        }
+
+        template <uint<4> N>
+        constexpr decltype(auto) get() {
+            return static_cast<field_selector<N, T...>&>(*this);
+        }
+
+        constexpr bool operator==(const message & other) const {
+            return ((static_cast<const T&>(*this) == static_cast<const T&>(other)) && ...);
+        }
+
+        constexpr bool operator!=(const message & other) const {
+            return !(*this == other);
+        }
+
     };
 }
 
--- test/message.cpp
+++ test/message.cpp
@@ -5,3 +5,8 @@
 using namespace pp;
 using namespace std;
 
+GTEST_TEST(message, function) {
+    message<integer_field<1, int>, float_field<3, float>> m{12, 1.23};
+    EXPECT_EQ(m.get<1>(), 12);
+    EXPECT_FLOAT_EQ(m.get<3>().value(), 1.23);
+}
