6264ff3ca08409aa9670d8d09932310ec44f6664
Twice <i@twice.moe>
2021-06-22 13:35:46 +0800

    reflection: support lvalue reference of message (#26)

--- include/protopuf/reflection.h
+++ include/protopuf/reflection.h
@@ -63,7 +63,45 @@ namespace pp {
     template <template<typename> typename Prop, field_c T>
     using dynamic_key_type = typename dynamic_key_type_impl<Prop, T>::type;
 
-    /// @brief Apply a function `f` to a field found by a property with run-time value `key` in a message `msg`
+    template <template<typename> typename Prop, typename F, template <typename ...> typename M, typename... Ts>
+        requires std::conjunction_v<std::bool_constant<std::invocable<F, Ts>>...> && message_c<std::remove_cv_t<M<Ts...>>>
+    struct dynamic_visit_impl {
+
+        using message_type = M<Ts...>;
+        using result_type = std::common_type_t<std::invoke_result_t<F, Ts>...>;
+        using key_type = dynamic_key_type<Prop, type_get_first<Ts...>>;
+
+        static inline const std::unordered_map <key_type, std::function<
+            result_type (F&&, message_type&)
+        >> reflect_map = {
+            {key_type { Prop<Ts>::value }, [](F&& f, message_type& msg){
+                return std::forward<F>(f)(msg.template get<Prop<Ts>::value>());
+            }}...
+        };
+
+        static auto visit(F&& f, message_type& msg, const key_type& key) {
+            if(auto iter = reflect_map.find(key); iter != reflect_map.end()) {
+                if constexpr (std::is_same_v<result_type, void>) {
+                    iter->second(std::forward<F>(f), msg);
+                    return true;
+                } else {
+                    return std::optional{iter->second(std::forward<F>(f), msg)};
+                }
+            }
+
+            if constexpr (std::is_same_v<result_type, void>) {
+                return false;
+            } else {
+                return std::optional<result_type>{};
+            }
+        }
+    };
+
+    /// A const wrapper of @ref message
+    template <typename ...T>
+    using const_message = const message<T...>;
+
+    /// @brief Apply a function `f` to a field found by a property with run-time value `key` in a const-qualified message `msg`
     /// 
     /// Template parameters:
     /// @param Prop the getter, which can be @ref field_name or @ref field_number
@@ -73,45 +111,34 @@ namespace pp {
     /// @param msg the message where the field is found 
     /// @param key the run-time value to find the specific field in the message
     template <template<field_c> typename Prop, typename F, field_c ... Ts>
-        requires std::conjunction_v<std::bool_constant<std::invocable<F, Ts>>...>
     auto dynamic_visit_by(F&& f, const message<Ts...>& msg, const dynamic_key_type<Prop, type_get_first<Ts...>>& key) {
-        using result_type = std::common_type_t<std::invoke_result_t<F, Ts>...>;
-        using key_type = dynamic_key_type<Prop, type_get_first<Ts...>>;
-
-        static const std::unordered_map <key_type, std::function<
-            result_type (F&&, const message<Ts...>&)
-        >> reflect_map = {
-            {key_type { Prop<Ts>::value }, [](F&& f, const message<Ts...>& msg){
-                return std::forward<F>(f)(msg.template get<Prop<Ts>::value>());
-            }}...
-        };
-
-        if(auto iter = reflect_map.find(key); iter != reflect_map.end()) {
-            if constexpr (std::is_same_v<result_type, void>) {
-                iter->second(std::forward<F>(f), msg);
-                return true;
-            } else {
-                return std::optional{iter->second(std::forward<F>(f), msg)};
-            }
-        }
-
-        if constexpr (std::is_same_v<result_type, void>) {
-            return false;
-        } else {
-            return std::optional<result_type>{};
-        }
+        return dynamic_visit_impl<Prop, F, const_message, Ts...>::visit(std::forward<F>(f), msg, key);
+    }
+    
+    /// @brief Apply a function `f` to a field found by a property with run-time value `key` in a non-const message `msg`
+    /// 
+    /// Template parameters:
+    /// @param Prop the getter, which can be @ref field_name or @ref field_number
+    /// 
+    /// Function parameters:
+    /// @param f the callback function to be invoked
+    /// @param msg the message where the field is found 
+    /// @param key the run-time value to find the specific field in the message
+    template <template<field_c> typename Prop, typename F, field_c ... Ts>
+    auto dynamic_visit_by(F&& f, message<Ts...>& msg, const dynamic_key_type<Prop, type_get_first<Ts...>>& key) {
+        return dynamic_visit_impl<Prop, F, message, Ts...>::visit(std::forward<F>(f), msg, key);
     }
 
     /// Apply a function `f` to a field found by run-time field `name` in a message `msg`, same as `dynamic_visit_by<field_name>`
-    template <typename F, message_c T>
-    auto dynamic_visit_by_name(F&& f, const T& msg, const auto& name) {
-        return dynamic_visit_by<field_name>(std::forward<F>(f), msg, name);
+    template <typename F,  typename T> requires message_c<std::remove_cvref_t<T>>
+    auto dynamic_visit_by_name(F&& f, T&& msg, const auto& name) {
+        return dynamic_visit_by<field_name>(std::forward<F>(f), std::forward<T>(msg), name);
     }
 
     /// Apply a function `f` to a field found by run-time field `number` in a message `msg`, same as `dynamic_visit_by<field_number>`
-    template <typename F, message_c T>
-    auto dynamic_visit_by_number(F&& f, const T& msg, const auto& number) {
-        return dynamic_visit_by<field_number>(std::forward<F>(f), msg, number);
+    template <typename F, typename T> requires message_c<std::remove_cvref_t<T>>
+    auto dynamic_visit_by_number(F&& f, T&& msg, const auto& number) {
+        return dynamic_visit_by<field_number>(std::forward<F>(f), std::forward<T>(msg), number);
     }
 }
 
